4ef7d963bd20834fc60ba7938b5451882e71e6eb
Johannes1803 <Johannes.Birk11@web.de>
2016-05-17 14:44:16 +0200

    Add list of places using knockout observables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// array of locations
 var locations = [
     {
         name:"Hain",
@@ -11,19 +12,19 @@ var locations = [
         name:"Sandstrasse",
         position: {lat: 49.89168, lng:  10.88491}
     }
-    ]
+    ];
 
-
+// initialize the ap with markers of my locations on it
 var initMap = function initMap() {
     var mapDiv = document.getElementById('map');
-    var myLatLng = {lat: 49.89565, lng: 10.88444}
+    var myLatLng = {lat: 49.89565, lng: 10.88444};
 
     var map = new google.maps.Map(mapDiv,{
         center: myLatLng,
         zoom: 16
     });
     var location, i, marker;
-
+    // loop over locations and add marker for each
     for (i = 0; i < locations.length; i++) {
         location = locations[i];
         marker = new google.maps.Marker({
@@ -40,4 +41,11 @@ var initMap = function initMap() {
         title: 'Hello World!'
     });*/
 
-}
+};
+
+var ViewModel = function() {
+    this.firstName = "Bert";
+    this.placelist = ko.observableArray(locations);
+};
+
+ko.applyBindings(new ViewModel());
