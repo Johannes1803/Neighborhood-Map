91072270b7be1374f41fc74d0af39a5fb1c095f8
Johannes1803 <Johannes.Birk11@web.de>
2017-01-23 16:07:44 +0100

    Add Error Handler for Google Maps
    Handle Cases where Maps Api does not respond and alert user.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-// array of locations
+var cityStr = "Bamberg";
+var WIKI_ERROR = "Sorry, Wikipedia does not respond!";
+var GOOGLE_ERROR = "Failed to load Google API :(";
+var WAITING_TIME = 8000;
+
+
+/**array of locations **/
 var locations = [
     {
-        name:"Künstlerhaus Villa Concordia",
-        position: {lat: 49.88521, lng: 10.89405}
+        name:"Otto-Friedrich Universität",
+        position: {lat: 49.893860, lng: 10.886130}
     },
     {
         name:"E.T.A. Hoffmann Theater",
@@ -11,121 +17,135 @@ var locations = [
     {
         name:"Schlenkerla",
         position: {lat: 49.89168, lng:  10.88491}
+    },
+    {
+        name: "Altes Rathaus Bamberg",
+        position: {lat: 49.891594, lng: 10.886843}
+    },
+    {
+        name: "Bamberger Dom",
+        position: {lat: 49.890775, lng: 10.882515}
     }
     ];
 
-// initialize the map with markers of my locations on it
-var map, marker, chosen_marker, call_wiki_api, articledescription;
+/** Variables defined in global scope **/
+var map, marker, chosen_marker, call_wiki_api, infowindow, prev_marker;
 
-	call_wiki_api = function(chosen_marker){
-		//console.log(list_place);
-		var place_clicked = chosen_marker.title;
-		//google.maps.event.trigger(list_place.marker,'click');
-		//console.log(place_clicked);
-		var wiki_url = "https://de.wikipedia.org/w/api.php?action=opensearch&search=" + place_clicked + "&format=json&callback=wikiCallback";
-		
+    /** Function: take as input a map marker, call an ajax request and show the answer from wiki api 
+    in a google maps infowindow **/
+    call_wiki_api = function(chosen_marker){
+        var place_clicked = chosen_marker.title;
+        var wiki_url = "https://de.wikipedia.org/w/api.php?action=opensearch&search=" + place_clicked +
+        "&format=json&callback=wikiCallback";
+        // error handler
         var wikiRequestTimeout = setTimeout(function(){
-            articledescription = "Error: Wiki cant be reached!"
-            }, 8000);
+            alert (WIKI_ERROR);
+            infowindow.setContent(WIKI_ERROR);
+            infowindow.open(map, chosen_marker);
 
+            }, WAITING_TIME);
+        // ajax request
         $.ajax({
-		url: wiki_url,
-		dataType: "jsonp",
-		}).done(function(response){
-			//console.log(response);
-		    var articlelist = response[1];
-		    var description_list = response[2];
-		    //console.log(articlelist);
-		    var articleStr = articlelist[0];
-		    articledescription = description_list[0]
-            console.log(articledescription);
-		    var url = "http://de.wikipedia.org/wiki/"+ articleStr + " Bamberg";
+          url: wiki_url,
+          dataType: "jsonp",
+        }).done(function(response){
+            var articlelist = response[1];
+            var description_list = response[2];
+            var articleStr = articlelist[0];
+            var articledescription = description_list[0];
+            var url = '<a href ="http://de.wikipedia.org/wiki/'+ articleStr + '">' + articleStr + '</a>';
+            var final_content = url + "<br>" + articledescription;
 
+            // show the response in the infowindow
+            infowindow.setContent(final_content);
+            infowindow.open(map, chosen_marker);
+            // dismiss error message
             clearTimeout(wikiRequestTimeout);
         });
-        return articledescription;
 
+    };
 
-	};
+/** If Google Maps fails to laod, inform the user **/
+var googleError = function(){
+    alert (GOOGLE_ERROR);
+};
 
-
+/** Function: initialize the map, add map markers and event listeners to each **/
 var initMap = function initMap() {
     var mapDiv = document.getElementById('map');
     var myLatLng = {lat: 49.89260, lng: 10.88454};
 
     map = new google.maps.Map(mapDiv,{
         center: myLatLng,
-        zoom: 14
+        zoom: 15
     });
     // Initialize InfoWIndow. There is only one
-    var infowindow = new google.maps.InfoWindow({
+    infowindow = new google.maps.InfoWindow({
         content: ""
     });
+    // value of previous marker initially set to 'null'
+    prev_marker = null;
 
+    // loop over array of locations and create marker for each
+    locations.forEach(function(location) {
+        location.marker = new google.maps.Marker({
+            position: location.position,
+            map: map,
+            title: location.name,
 
-    /*markerx = new google.maps.Marker({
-      map: map,
-      draggable: true,
-      animation: google.maps.Animation.DROP,
-      position: {lat: 49.88327, lng: 10.8867}
-    });
-    markerx.addListener('click', toggleBounce);*/
-
-	locations.forEach(function(location) {
-    	location.marker = new google.maps.Marker({
-    		position: location.position,
-    		map: map,
-    		title: location.name,
-
-    	});
+        });
         chosen_marker = location.marker;
-        var title = chosen_marker.title;
-        //console.log(chosen_marker);
-        google.maps.event.addListener(chosen_marker,'click', (function(markercopy) {
+
+        // add event listener for marker animation to every marker
+        google.maps.event.addListener(chosen_marker,'click', (function(chosen_marker) {
             return function(){
-                if (markercopy.getAnimation() == null) {
-                    markercopy.setAnimation(google.maps.Animation.BOUNCE);
+                if(prev_marker) {
+                    // keep track of prev_marker to have only one
+                    // bouncing at a time
+                  if (prev_marker.getAnimation() !== null) {
+                      prev_marker.setAnimation(null);                               
+                      chosen_marker.setAnimation(google.maps.Animation.BOUNCE);
+                      prev_marker = chosen_marker;
+                  }
                 } else {
-                    markercopy.setAnimation(null);
-                };
-            }
+                  chosen_marker.setAnimation(google.maps.Animation.BOUNCE);
+                  prev_marker = chosen_marker;
+
+                }
+            };
         })(chosen_marker));
 
-        
 
+        // add event listener for calling wiki api to every marker
         google.maps.event.addListener(chosen_marker, 'click', (function(chosen_marker) {
-        	return function(){
-        		var wiki_response = call_wiki_api(chosen_marker);
-                console.log(wiki_response);
-                infowindow.setContent(wiki_response);
-                //console.log(infowindow.content);
-                infowindow.open(map, chosen_marker);
-                //infowindow.open(map, chosen_marker);
+            return function(){
+                call_wiki_api(chosen_marker);
 
-        	};
+            };
         })(chosen_marker));
 
     });
-    //console.log(chosen_marker);
-    
-	ko.applyBindings(new ViewModel());
-};
-// ko from here on 
 
-// Class of places
+    /** Knockout from now on: KO initiated from within initMap. **/
+    ko.applyBindings(new ViewModel());
+};
+
+
+/** Class of Places**/
 var Place = function(data) {
     this.name = data.name;
     this.position = data.position;
-    // remove the assignment in line 47 HEIDI!!!
     this.marker = data.marker;
 };
 
+/** View Model**/
 var ViewModel = function() {
     // trick to make sure this refers to the right binding
     var self = this;
-
+    // store input typed by user
     this.query = ko.observable('');
 
+    // observable array to add places to
     this.placelist = ko.observableArray([]);
 
     // loop over locations and add objects to the 
@@ -133,30 +153,30 @@ var ViewModel = function() {
     locations.forEach(function(place){
         self.placelist.push(new Place(place));
     });
-    // console.log(this.showlist);
+    /** Filter Markers and list elements according to user input**/
     // computed to look for user input and perform search
+    // computed gets invoked when user types
     this.showlist = ko.computed(function() {
-        // computed gets invoked when user types
         var filter = self.query().toLowerCase();
         // arrayFilter returns places whose names match the search query
         return ko.utils.arrayFilter(self.placelist(), function(prod) {
             var current_name = prod.name.toLowerCase();
             // store the result of comparison in visible
             var visible = current_name.indexOf(filter)!== -1;
-            // update visibility of marker
+            //the list of matches of substrings is displayed
             prod.marker.setVisible(visible);
             return visible;
-            //the list of matches of substrings is displayed
         });
     });
-    // Clicking a list element simulates a click on the corresponding marker
+
+    /** If user clicks on a list element, simulate a click on
+    the corresponding marker**/
     this.simulate_marker_clicked = function(list_place){
-    	google.maps.event.trigger(list_place.marker, 'click');
-    }
+        google.maps.event.trigger(list_place.marker, 'click');
+    };
 
 
 };
 
-var cityStr = "Bamberg";
 
 
